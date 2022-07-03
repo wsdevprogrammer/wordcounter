0c5163b54e348bbfba53ac7be38cd7e268b286b1
Waseem Saleem <64418567+wsdmarketer@users.noreply.github.com>
2022-07-03 16:52:18 +0500

    updated home link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,10 +12,10 @@ export default function navbar(props) {
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
           <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="/">{props.Home}</a>
+            <a className="nav-link active" aria-current="page" href="https://wsdmarketer.github.io/reactWordCounter/">{props.Home}</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="../Pages/About.js/">{props.About}</a>
+            <a className="nav-link" href="#">{props.About}</a>
           </li>
           </ul>
         <form className="d-flex" role="search">
@@ -36,5 +36,5 @@ navbar.propTypes={
 }
 
 navbar.defaultProps ={
-    title: "Your Blog Name"
+    title: "Word Counter"
 }
