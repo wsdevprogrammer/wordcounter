40cfcffc86ebdef4f8f96dba500fd2ac52110d36
Waseem Saleem <64418567+wsdmarketer@users.noreply.github.com>
2022-10-02 07:36:32 +0500

    Updates

---
+++ precache-manifest.1e7a95d8b518206618439b9af4836697.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed9e70be5e134b10fb63",
+    "url": "/reactWordCounter/static/css/main.ea7fc97e.chunk.css"
+  },
+  {
+    "revision": "ed9e70be5e134b10fb63",
+    "url": "/reactWordCounter/static/js/main.ed9e70be.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/reactWordCounter/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "1fb84335d4ce8f26b1b6",
+    "url": "/reactWordCounter/static/js/2.1fb84335.chunk.js"
+  },
+  {
+    "revision": "ec5e265b185f709792bc",
+    "url": "/reactWordCounter/static/js/runtime~main.ec5e265b.js"
+  },
+  {
+    "revision": "0e0d49060b9e24adeaf15c4a2a3691a4",
+    "url": "/reactWordCounter/index.html"
+  }
+];
