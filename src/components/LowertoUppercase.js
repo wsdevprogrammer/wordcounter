597ed0dfd43e6db11249714897c213ae6b96c04b
futurewebprogramming <123332352+futurewebprogramming@users.noreply.github.com>
2023-11-09 13:30:02 +0500

    Updated

--- src/components/LowertoUppercase.js
+++ src/components/LowertoUppercase.js
@@ -1,41 +1,38 @@
 import React, { useState } from "react";
 
-// import PropTypes from "prop-types";
-export default function LowertoUppercase(props) {
-    function handleUpClick() {
-        // console.log("Button Clicked " + text);
-        let text;
-        let newText = text.trim();
-        let changetext = newText.toUpperCase();
-        setText(changetext);
-        // setText("You hit the Button to Make Some Majic")
-    }
-    function handleOnChange(event) {
-        // console.log("Handle On Change");
-        setText(event.target.value);
+export default function LowertoUppercase() {
+    const [text, setText] = useState("Paste your Text here..");
+
+    const handleUpClick = () => {
+      const newText = text.trim();
+      const changedText = newText.toUpperCase();
+      setText(changedText);
     };
 
-    const handleCamelStyle = (event) => {
-        // console.log("Button Clicked " + text);
-        let cameltext = newText.toLowerCase();
-        setText(cameltext);
+    const handleCamelStyle = () => {
+      const camelText = text.toLowerCase();
+      setText(camelText);
     };
 
-    const clearText = (event) =>{
-        setText('');
+    const clearText = () => {
+      setText('');
     };
 
+    const handleOnChange = (event) => {
+      setText(event.target.value);
+    };
 
-    const [newText, setText] = useState("Remove Me 😍!.");
+    const wordCount = text.trim().split(/\s+/).filter(Boolean).length;
+    const charCount = text.trim().replace(/\s+/g, "").length;
+
     return (
         <>
-            
             <p className="mt-4" style={{color: 'gray'}}>Clear Text & Paste your Text in the Text Area</p>
             <div className="form-floating mb-3">
                 <textarea
                     className="form-control"
                     onChange={handleOnChange}
-                    value={newText}
+                    value={text}
                     rows="8"
                     placeholder="Leave a comment here"
                     id="floatingTextarea2"
@@ -43,7 +40,7 @@ export default function LowertoUppercase(props) {
                 ></textarea>
             </div>
             <button className="btn btn-primary cpata mx-2 " onClick={handleUpClick}>
-                Convert to Cpatalize
+                Convert to Capitalize
             </button>
             <button className="btn btn-primary camel mx-2 my-2" onClick={handleCamelStyle}>
                 Convert to Lowercase
@@ -52,23 +49,19 @@ export default function LowertoUppercase(props) {
             <button className="btn bg-warning btn btn-info mx-2 my-2" onClick={clearText}>
                 Clear
             </button>
-         
-        
 
             <div className="container mt-4">
-                {/* <h4>Your Text Summry</h4> */}
-                
-                <p>{newText.split(" ").length} Words {newText.length} Charachters</p>
+                <p>{wordCount} Words {charCount} Characters</p>
                 <hr className="w-25" />
                 <h3>Reading Time</h3>
                 <hr className="w-25" />
-                <p>{0.04 * newText.split("").length} Mintues Requried to Read </p>
+                <p>{0.04 * charCount} Minutes Required to Read</p>
                 <hr className="w-25" />
             </div>
 
             <div className="container">
                 <h3>Preview</h3>
-                <p>{newText}</p>
+                <p>{text}</p>
             </div>
         </>
     );
