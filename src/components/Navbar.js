e4c48641f21e74fcb2c236be493143268fef81f4
futurewebprogramming <123332352+futurewebprogramming@users.noreply.github.com>
2023-11-09 13:58:08 +0500

    Nav updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,17 +5,17 @@ export default function navbar(props) {
   return (
     <nav className="navbar navbar-expand-lg bg-primary">
     <div className="container ">
-      <a className="navbar-brand  text-white"  href="/">{props.title}</a>
+      <a className="navbar-brand  text-white"  href="/wordcounter">{props.title}</a>
       <button className="navbar-toggler text-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation text-white">
         <span className="navbar-toggler-icon text-white"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
           <li className="nav-item">
-            <a className="nav-link active  text-white" aria-current="page" href="/">{props.Home}</a>
+            <a className="nav-link active  text-white" aria-current="page" href="/wordcounter">{props.Home}</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link  text-white" href="https://wsdevprogrammer.github.io/password-generator-free/" target='new'>Free Password Genrator</a>
+            <a className="nav-link  text-white" href="/password-generator-free" target='new'>Free Password Genrator</a>
           </li>
           </ul>
       
